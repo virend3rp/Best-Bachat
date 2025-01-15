d015b06e41a7f466c8d9243680f78c7bb540858c
Virender Parasariya <parasariya.1@iitj.ac.in>
2025-01-15 21:09:55 +0530

    Rough stage  of the project

---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+import HeroCarousel from "@/components/HeroCarousel"
+import Searchbar from "@/components/Searchbar"
+import Image from "next/image"
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
+
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center"> 
+          <h1 className="head-text">
+                    <span className="text-green-500 inline">Unleash the Power of </span>
+                    <span className="text-primary inline">BACHAT</span>
+          </h1>
+            <p className="mt-6">
+            Track prices, find the best deals, and make smarter purchasing decisions.
+            </p>
+
+            <Searchbar />
+          </div>
+
+          <HeroCarousel />
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Your Products</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Home
